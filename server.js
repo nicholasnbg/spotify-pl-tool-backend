46773931c9afa06d6b7f45ca22558c2747e063c6
Nick Gray <nick.gray@rea-group.com>
2019-04-06 20:20:36 +1100

    setup routes for spotify auth

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const querystring = require('querystring');
-const dotenv = require('dotenv');
+const request = require('request');
+require('dotenv').config();
 
 const app = express();
 const port = 8888;
+
+const redirect_uri = 'http://localhost:8888/callback';
+
+app.get('/login', (req, res) => {
+  console.log("login hit");
+  res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: process.env.SPOTIFY_CLIENT_ID,
+        scope: 'user-read-private user-read-email',
+        redirect_uri
+      }))
+});
+
+app.get('/callback', (req, res) => {
+  const code = req.query.code || null;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+          process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+      ).toString('base64'))
+    },
+    json: true
+  };
+  request.post(authOptions, function(error, response, body) {
+    const access_token = body.access_token;
+    const uri = process.env.FRONTEND_URI || 'http://localhost:3000';
+    res.redirect(uri + '?access_token=' + access_token)
+  })
+});
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`)
